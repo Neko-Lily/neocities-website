9331c106f383957f0a09b4168f55b3dbe2684472
Lily Moon <142747233+Neko-Lily@users.noreply.github.com>
2025-07-10 23:13:24 +0200

    Changed up look a bit more.
    Changed height of: Panels, Chatbox
    Changed width of: Chatbox, Top Panel, Panel-Right
    Added more functionality to script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-
+const buttonReturnHome = document.getElementById("home-return")
 const buttonGaming = document.getElementById("gaming")
 const buttonProject = document.getElementById("project")
 const contentDiv = document.getElementById("scrollbar-js")
 
 
 
+document.addEventListener("DOMContentLoaded", function loadStandardContent() {
+    contentDiv.innerHTML = 
+    `
+        <img src="/images/animegirl-pfp.jpg">  
+        <img src="/images/catgirl-pfp.jpg">
+    `;
+});
+
+buttonReturnHome.addEventListener("click", function returnHome() {
+    document.location.href = 'https://neko-lily.neocities.org/'
+});
 
 buttonGaming.addEventListener("click", function changeInfoGaming () {
     contentDiv.innerHTML = 
@@ -22,6 +33,7 @@ buttonProject.addEventListener("click", function changeInfoProject(){
 });
 
 document.onload = function(){
+    loadStandardContent();
     changeInfoGaming();
     changeInfoProject();
 };
